0793126ad0fe04a91dc95ebe329105cefcbc0cc3
Humbert Dominguez <humbertodelac13@gmail.com>
2024-10-31 21:21:25 -0600

    sistema principal de navegacion agregado

--- app/src/main/java/com/example/donatec/MainActivity.java
+++ app/src/main/java/com/example/donatec/MainActivity.java
@@ -1,24 +1,34 @@
 package com.example.donatec;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        BottomNavigationView menu = findViewById(R.id.menu);
+        NavHostFragment principalFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.principalFragment);
+
+        if (principalFragment != null) {
+            NavController navController = principalFragment.getNavController();
+            NavigationUI.setupWithNavController(menu, navController);
+        } else {
+            Log.e("MainActivity", "principalFragment is null");
+        }
     }
 }

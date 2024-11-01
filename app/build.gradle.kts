618b9fd583aa010f2b7a357fa55dc40a6773fe9d
Humbert Dominguez <humbertodelac13@gmail.com>
2024-10-31 20:30:53 -0600

    configuraciones iniciales agregadas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -37,6 +40,16 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation ("androidx.navigation:navigation-fragment:2.7.1")
+    implementation ("androidx.navigation:navigation-ui:2.7.1")
+    implementation ("com.google.android.material:material:1.9.0")
+    implementation("com.android.volley:volley:1.2.1")
+    implementation("com.squareup.picasso:picasso:2.8")
+    implementation("com.google.android.gms:play-services-maps:18.1.0")
+    implementation("com.google.android.gms:play-services-location:21.0.1")
+    implementation(libs.annotation)
+    implementation(libs.lifecycle.livedata.ktx)
+    implementation(libs.lifecycle.viewmodel.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
